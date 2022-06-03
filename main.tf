b6f2e5a9873199c3683c74e9d4b37921396aa17e
gopi350 <gopikrishnadbda17@gmail.com>
2022-06-03 09:32:42 +0530

    again  modified main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "google_compute_network" "vpc_network" {
   
   name="terraform-network"
 }
-# we are creating another network (VPC@)
-resource "google_compute_network" "vpc_network" {
+# we are creating another network (VPC2)
+resource "google_compute_network" "gopi_network" {
   
   name="my-network"
 }
